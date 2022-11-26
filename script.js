1e00a1d3a2055902eece623e7ce612afde7b8956
KLC <mkelsy@icloud.com>
2022-11-25 21:04:35 -0500

    Displaying photos

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
 const imageContainer = document.getElementById('image-container');
-
-const count = 3;
+const loader = document.getElementById('loader');
+const count = 5;
 const apiKey = 'tcCJC8Q_ZAbZxu6R98dZDirzEsD3HZc1GSqa2H6Q4vg';
-const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
-const images = [0]
+let photoArrays = [];
 
-async function getApi() {
-	const response = await fetch(apiUrl);
-	const data = await response.json();
+// Helper function to set attributes
 
-	console.log(data);
+function setAttributes(element, attributes) {
+	for (const key in attributes) {
+		element.setAttribute(key, attributes[key])
+	}
 }
 
 
+function displayPhotos() {
+photoArrays.forEach((photo) => {
+
+	const item = document.createElement('a');
+	setAttributes(item, {
+		href: photo.links.html,
+		target: '_blank'
+	})
+	const thePhotos = document.createElement('img');
+	setAttributes(thePhotos, {
+		src: photo.urls.regular,
+		title: 'Click For More',
+		alt: 'Click For More'
+	})
+
+	item.appendChild(thePhotos);
+	imageContainer.appendChild(item);
+})
+} 
+
+async function getApi() {
+	try {
+		const response = await fetch(apiUrl);
+		photoArrays = await response.json();
+		console.log(photoArrays);
+		displayPhotos();
+	} catch (error) {
+
+	}
+}
 
 getApi();
+
+
+
 
